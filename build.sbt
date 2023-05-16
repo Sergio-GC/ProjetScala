9c29daaf1053acb9187ed04f55a1ae998f2e6bd1
Bastien Salamin <bastien.salamin@hes-so.ch>
2023-05-16 15:08:21 +0200

    Première lecture fichier CSV

--- build.sbt
+++ build.sbt
@@ -1,1 +1,8 @@
+ThisBuild / version := "1.0"
 ThisBuild / scalaVersion := "3.1.3"
+ThisBuild / organization := "ch.hevs"
+
+libraryDependencies ++= Seq(
+  "org.scalatest" %% "scalatest" % "3.2.14" % Test,
+  "com.github.tototoshi" %% "scala-csv" % "1.3.8"
+)
--- src/main/scala/Demo.scala
+++ src/main/scala/Demo.scala
@@ -1,3 +1,10 @@
 package demo
 
-@main def hello() = println("Hello world")
+import com.github.tototoshi.csv._
+import java.io.File
+
+@main def hello() = {
+    val reader = CSVReader.open(new File("data/07-ShelterDogs.csv"))
+    val rows = reader.all()
+    rows.foreach(row => println(row.mkString(", ")))
+}
